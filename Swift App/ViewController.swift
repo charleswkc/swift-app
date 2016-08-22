0af9c4b3f74fd7ab23a7b396aef81555dff7b1e0
Charles Wong <charles.wong@tetrapak.com>
2016-08-22 13:33:34 +0800

    My first commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Charles Wong on 20/8/16.
+//  Copyright © 2016 Charles Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0 //start count from zero
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+        theLabel.text = "Hello there!" //change label when button tapped
+        print("Button tapped") //print at dev console
+        tapCount = tapCount + 1 //go up +1 each time button tapped
+        print(tapCount) //print the count
+        if tapCount > 10 { //change the label when tapped > 10 times
+            theLabel.text = "You tapped the button 10 times!"
+        }
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //self.view.backgroundColor = UIColor.red //make background red
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
